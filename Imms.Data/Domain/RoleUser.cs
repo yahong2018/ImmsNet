9a49cff16397bd19dc35f2812f0d9b5dee40c750
yahong2018 <SoftwareChina@qq.com>
2019-03-19 15:57:01 +0800

    1.数据库表的第一版设计  2.业务实体类脚手架代码编写

--- Imms.Data/Domain/RoleUser.cs
+++ Imms.Data/Domain/RoleUser.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 namespace Imms.Data.Domain
 {
     public partial class RoleUser:Entity<long>
-    {
-        public long RecordId { get; set; }
+    {        
         public long RoleId { get; set; }
         public long UserId { get; set; }
     }
