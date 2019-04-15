b6af293bcf6db161ae1e0a711706f04443e40464
yahong2018 <SoftwareChina@qq.com>
2019-04-15 08:44:34 +0800

    增加移动端,但发现vscode无法开发Xamrin程序

---
+++ Imms.App/Imms.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Imms.App.Models;
+
+namespace Imms.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
